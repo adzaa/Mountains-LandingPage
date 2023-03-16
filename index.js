1cd4e4246f566d862eb95b27f9eb6fa473c427f6
adzaa <adidzankic@gmail.com>
2023-03-16 22:23:27 +0100

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+Hello my name is Abdulah. I am 20 years old. and I come from Bosnia and Herzegovina. 
+I remember when I was kid and got first computer. When I saw that google logo I was so impressed
+like someone gave me a candy I dont know why but I remember that for some reason.
+and specififili in this industry i dont know. I tried everything from music producing to working in a coffe 
+shop but developing something making someones idea alive coding that is what I chosed.
+
+First of all taht is a new experience for me, and what most exites me is meeting 
+new koligs new working environment and I believe that I will learn a lot more with you.Abdulah
+
+
+I am very persistent. if I start something, I have to finish it, even if I sit for 24 hours.
+I believe I do have good qualifications and knowledge for your position
+
+I’m a great team player,
+  I end up making friend fast. 
